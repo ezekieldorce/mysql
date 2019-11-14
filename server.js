fdc6dc5dc00e384901979f99e5863656ac015a63
Ezekiel <ezekieldorce@gmail.com>
2019-11-14 13:24:59 -0500

    fixed server

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
 const express = require('express');
 const app = express();
-console.log("server is up on port 3000");
+app.listen(3000, function () {
+    console.log("server is up on port 3000");
+});
